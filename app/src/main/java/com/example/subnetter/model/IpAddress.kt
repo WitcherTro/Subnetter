8403ca7a32545c8bc9393e042093a5976fb347ee
WitcherTro <miskosihelsky1@gmail.com>
2024-06-10 22:57:43 +0200

    added data model classes

---
+++ app/src/main/java/com/example/subnetter/model/IpAddress.kt
@@ -0,0 +1,8 @@
+package com.example.subnetter.model
+
+data class IpAddress(
+    val octet1: Int,
+    val octet2: Int,
+    val octet3: Int,
+    val octet4: Int
+)
---
+++ app/src/main/java/com/example/subnetter/model/NetworkInformation.kt
@@ -0,0 +1,9 @@
+package com.example.subnetter.model
+
+data class NetworkInformation(
+    val networkAddress: IpAddress,
+    val broadcastAddress: IpAddress,
+    val firstUsableAddress: IpAddress,
+    val lastUsableAddress: IpAddress,
+    val numberOfHosts: Int
+)
