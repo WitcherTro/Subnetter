28b59c6c91ceca7af22900f1fa4b70d9ca83bbf3
WitcherTro <miskosihelsky1@gmail.com>
2024-06-10 22:26:13 +0200

    edited ipaddress components to use BasicTextField

--- app/src/main/java/com/example/subnetter/ui/IpAddress.kt
+++ app/src/main/java/com/example/subnetter/ui/IpAddress.kt
@@ -3,71 +3,81 @@ package com.example.subnetter.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun IpAddressInput(
     modifier: Modifier = Modifier,
-    onValueChange: (List<String>) -> Unit
+    onValueChange: (List<String>) -> Unit,
 ) {
     val octets = remember { List(4) { mutableStateOf("") } }
 
     Row(modifier = modifier) {
         for (i in octets.indices) {
-            TextField(
-                value = octets[i].value,
-                onValueChange = { newValue: String ->
-                    if (newValue.isEmpty() || newValue.toIntOrNull() in 0..255) {
-                        octets[i].value = newValue
-                        onValueChange(octets.map { it.value })
-                    }
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                singleLine = true,
-                maxLines = 1,
-                modifier = Modifier.width(65.dp)
-            )
+            Box(modifier = Modifier.background(Color.LightGray)){
+                BasicTextField(
+                    value = octets[i].value,
+                    onValueChange = { newValue: String ->
+                        if (newValue.isEmpty() || newValue.toIntOrNull() in 0..255) {
+                            octets[i].value = newValue
+                            onValueChange(octets.map { it.value })
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+                    singleLine = true,
+                    modifier = Modifier.width(65.dp)
+                        .height(32.dp)
+                        .padding(7.dp),
+                    textStyle = TextStyle(textAlign = TextAlign.Center)
+                )
+            }
             if (i < octets.size - 1) {
-                Text("⬤", modifier = Modifier.padding(horizontal = 4.dp).offset(y = 32.dp))
+                Text("⬤", modifier = Modifier.padding(horizontal = 4.dp).offset(y = 14.dp))
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IpAddressOutput(
     modifier: Modifier = Modifier,
     value: List<String>,
-    backgroundColor: Color = Color.DarkGray,
+    backgroundColor: Color = Color.Gray,
 ) {
     val octets = remember { value.map { mutableStateOf(it) } }
 
     Row(modifier = modifier) {
         for (i in octets.indices) {
-            Box(modifier = Modifier.background(backgroundColor)) {
-                TextField(
+            Box(modifier = Modifier.background(backgroundColor), contentAlignment = Alignment.Center) {
+                BasicTextField(
                     value = octets[i].value,
                     onValueChange = { },
                     readOnly = true,
                     modifier = Modifier.width(65.dp)
+                        .height(32.dp)
+                        .padding(7.dp),
+                    textStyle = TextStyle(textAlign = TextAlign.Center)
                 )
             }
             if (i < octets.size - 1) {
-                Text("⬤", modifier = Modifier.padding(horizontal = 4.dp).offset(y = 32.dp))
+                Text("⬤", modifier = Modifier.padding(horizontal = 4.dp).offset(y = 14.dp))
             }
         }
     }
