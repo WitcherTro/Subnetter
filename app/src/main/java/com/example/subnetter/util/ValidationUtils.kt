cfce762a6e90cfa21a9930ee3f4534771a23dea8
WitcherTro <miskosihelsky1@gmail.com>
2024-06-11 14:05:00 +0200

    added subnet mask validation

--- app/src/main/java/com/example/subnetter/util/ValidationUtils.kt
+++ app/src/main/java/com/example/subnetter/util/ValidationUtils.kt
@@ -8,3 +8,18 @@ fun isValidIpAddress(ipAddress: IpAddress): Boolean {
             ipAddress.octet3 in 0..255 &&
             ipAddress.octet4 in 0..255
 }
+
+fun isValidSubnetMask(subnetMask: IpAddress): Boolean {
+    val parts = listOf(subnetMask.octet1, subnetMask.octet2, subnetMask.octet3, subnetMask.octet4)
+
+    var binaryStr = ""
+    for (part in parts) {
+        if (part !in 0..255) return false
+        binaryStr += String.format("%08d", Integer.parseInt(Integer.toBinaryString(part)))
+    }
+
+    val firstZero = binaryStr.indexOf('0')
+    val lastOne = binaryStr.lastIndexOf('1')
+
+    return firstZero > lastOne
+}
